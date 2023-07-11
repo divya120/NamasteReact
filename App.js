69fe5bd79875c18453c809cbc1d623821737ad05
Divya Kulkarni <divya12.7a@gmail.com>
2023-07-11 12:09:39 +0530

    Ignited the project 🔥

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 // const heading = React.createElement("h1",{id : "h1-element", abc:"xyz"}, "Hello world using React");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // console.log(heading);
